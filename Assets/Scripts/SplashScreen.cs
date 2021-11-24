3f4b2a5e71c19abb9f813041ed031d2fb3602e14
FCWombat <furrycombatwombat@gmail.com>
2021-11-23 19:34:45 -0500

    Initial commit

---
+++ Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreen : MonoBehaviour
+{
+    public string NextScene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ChangeToScene(NextScene, 3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ChangeToScene(string sceneToChangeTo, float delay)
+    {
+        StartCoroutine(DoChangeScene(sceneToChangeTo, delay));
+    }
+    IEnumerator DoChangeScene(string sceneToChangeTo, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(sceneToChangeTo);
+    }
+}
